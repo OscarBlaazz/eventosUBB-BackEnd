54724c853ddc31119180f3e23c78c826e063cefb
OscarBlas <patoblas7@gmail.com>
2019-06-19 03:57:26 -0400

    model evento

--- app/Evento.php
+++ app/Evento.php
@@ -7,5 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Evento extends Model
 {
     protected $table ="evento";
-    protected $fillable =['nombre','ubicacion','direccion','detalles','imagen','capacidad'];
 }
